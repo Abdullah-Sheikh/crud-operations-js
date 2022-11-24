1dc97838a221ebfdab5eae4df8d19eeda7d739cf
Abdullah <abdullahsh123@hotmail.com>
2022-11-24 19:04:38 +0500

    Update student implemented

--- js/student.js
+++ js/student.js
@@ -3,7 +3,7 @@ const table = document.querySelector("table");
 const thead = document.querySelector("thead");
 const tbody = document.querySelector("tbody");
 const array = [];
-
+let rowIndex;
 
 const addStudent = () =>{
 
@@ -118,3 +118,86 @@ const getStudent = () =>{
 
 }
 
+table.onclick =() =>{
+
+    let row = table.rows;
+    for(let i=0 ; row.length ; i++)
+    {
+        row[i].addEventListener("click",activateItem);
+    }
+}
+
+
+function activateItem()
+{
+    rowIndex = this.rowIndex;
+    let name = document.querySelector("#name");
+    let age = document.querySelector("#age");
+    let city = document.querySelector("#city");
+
+
+    name.value = this.cells[0].innerText;
+    age.value = this.cells[1].innerText;
+    city.value = this.cells[2].innerText;
+
+    
+}
+
+
+const updateStudent = ()=>{
+
+    const storage = JSON.parse(localStorage.getItem("studentList"));
+    let name = document.querySelector("#name").value;
+    let age  = document.querySelector("#age").value;
+    let city = document.querySelector("#city").value;
+    let tableRowIndex = rowIndex ;
+
+    if(tableRowIndex--)
+    {
+        if(name==="")
+        {
+            alert("Name can not be left empty");
+            document.querySelector("#name").focus();
+            return;
+
+        }
+        if(age==="")
+        {
+            alert("Age can not left empty");
+            document.querySelector("#age").focus();
+            return;
+        }
+        if(city ==="")
+        {
+            alert("city can not left empty");
+            document.querySelector("#city").focus();
+            return;
+        }
+
+        let confirm = window.confirm("Are you sure you want to update the data?")
+
+        if(confirm)
+        {
+            storage &&
+            storage.splice(tableRowIndex ,1,{
+                name,
+                age,
+                city
+            });
+    
+            localStorage.setItem("studentList",JSON.stringify(storage));
+            console.log(storage);
+            window.alert("Data has been updated");
+            window.location.reload();
+    
+        }
+        else{
+            return;
+        }
+    }
+    else{
+        return;
+    }
+
+   
+}
